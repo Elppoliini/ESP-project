3dac6928a125eda7174fa8e345eaa45dbbc40ff8
Johanna Nygård <johanna.nygard@metropolia.fi>
2020-03-08 19:04:57 +0200

    delete everything to add the upp to date code

--- lpc_chip_15xx/Debug/src/ring_buffer.d
+++
@@ -1,7 +0,0 @@
-src/ring_buffer.o src/ring_buffer.d: ../src/ring_buffer.c \
- C:\Users\Elppo\Documents\Koulu\2.vuosi_Smart_Systems\3\ jakso\ES-project\lpc_chip_15xx\inc/ring_buffer.h \
- C:\Users\Elppo\Documents\Koulu\2.vuosi_Smart_Systems\3\ jakso\ES-project\lpc_chip_15xx\inc/lpc_types.h
-
-C:\Users\Elppo\Documents\Koulu\2.vuosi_Smart_Systems\3\ jakso\ES-project\lpc_chip_15xx\inc/ring_buffer.h:
-
-C:\Users\Elppo\Documents\Koulu\2.vuosi_Smart_Systems\3\ jakso\ES-project\lpc_chip_15xx\inc/lpc_types.h:
